73002b0310fcdebc314a56f77df2804fc7c63e56
Freddyede <136079691+freddyede@users.noreply.github.com>
2025-08-01 16:11:24 +0200

    Adding: - anonymous.routes.ts Modify: - app.component.html - app.routes.ts
    Signed-off-by: Franck <136079691+Freddyede@users.noreply.github.com>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: async () =>
+      import('./anonymous/anonymous-layout/anonymous-layout.component')
+        .then(c => c.AnonymousLayoutComponent),
+    loadChildren: async () => import('./anonymous/anonymous.routes').then(r => r.ANONYMOUS_ROUTES),
+  }
+];
