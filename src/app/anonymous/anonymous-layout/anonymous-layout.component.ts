16689654b00f1d57bc4d425ef9c3d45ea127f47f
Franck <136079691+Freddyede@users.noreply.github.com>
2025-08-01 16:12:34 +0200

    Adding: - anonymous.routes.ts - anonymous-layout.component.html - anonymous-layout.component.less - anonymous-layout.component.spec.ts - anonymous-layout.component.ts Modify: - app.component.html - app.routes.ts
    Signed-off-by: Franck <136079691+Freddyede@users.noreply.github.com>

---
+++ src/app/anonymous/anonymous-layout/anonymous-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-anonymous-layout',
+  imports: [],
+  templateUrl: './anonymous-layout.component.html',
+  styleUrl: './anonymous-layout.component.less'
+})
+export class AnonymousLayoutComponent {
+
+}
